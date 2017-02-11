767c558fb22aae26d4bb72b06783ecb01c4df62f
jose macias <joselmacs20@gmail.com>
2017-02-11 13:59:38 -0600

    Evaluación 2 proyectos de tarea

---
+++ FoundationScaffold/config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  resources :autos
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'autos#index'
+end
